dd9c5d9ed3ef9311071ab8d989ff3ca147a53f26
Léo Garret <leodu34@hotmail.com>
2019-04-01 21:11:59 +0200

    Step: separate operators for multiplication

--- Cpp_Learning/main.cpp
+++ Cpp_Learning/main.cpp
@@ -6,7 +6,8 @@ int main()
 	int a, b = 0;
 
 	cout << "Enter two numbers" << endl;
-	cin >> a >> b;
+	cin >> a;
+	cin >> b;
 	cout << "The result of " << a << " * " << b << " is " << a * b << "." << endl;
 	while (1);
 	return 0;

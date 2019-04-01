bf3bc5aa92b247f27854e4cd1cf01306802c5dab
Léo Garret <leodu34@hotmail.com>
2019-04-01 21:40:29 +0200

    Step: While statement exercices

--- Cpp_Learning/main.cpp
+++ Cpp_Learning/main.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main()
 {
-	int sum = 0, val = 1;
+	int sum = 0, val = 0, end = 0;
 
-	while (val <= 10) {
+	cout << "Choose two numbers to make a sequence and print the result of all the sequence' numbers" << endl;
+	cin >> val >> end;
+	while (val <= end) {
 		sum += val;
 		++val;
 	}
-	cout << "Sum of 1 to 10 inclusive is " << sum << "." << endl;
+	cout << "Sum of 10 to 0 not inclusive is " << sum << "." << endl;
 	while (1);
 	return 0;
 }

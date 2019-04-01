ba98aa816bc7d67067d9048b3a2d3215c5bf9326
Léo Garret <leodu34@hotmail.com>
2019-04-01 23:53:01 +0200

    Step: if statement exercice

--- Cpp_Learning/main.cpp
+++ Cpp_Learning/main.cpp
@@ -3,23 +3,17 @@ using namespace std;
 
 int main()
 {
-	int curVal = 0, val = 0;
+	int start = 0, end = 0;
+	int sum = 0;
 
-	if (cin >> val) {
-		int count = 0;
-
-		count = 1;
-		while (cin >> val) {
-			if (val == curVal)
-				++count;
-			else {
-				cout << curVal << " occurs " << count << " times." << endl;
-				curVal = val;
-				count = 1;
-			}
-		}
-		cout << curVal << " occurs " << count << " times." << endl;
+	while (start >= end) {
+		cout << "Enter two numer to make a sequence (start number is lesser than end number)." << endl;
+		cin >> start;
+		cin >> end;
 	}
+	for (; start <= end; start++)
+		sum += start;
+	cout << "The sum is: " << sum << endl;
 	while (1);
 	return 0;
 }

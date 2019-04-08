8334f0f90ad7f5157956bc4593aa3a6be7d71c03
Léo Garret <leodu34@hotmail.com>
2019-04-08 23:13:39 +0200

    Exercice: reference to pointer to int

--- Cpp_Learning/main.cpp
+++ Cpp_Learning/main.cpp
@@ -2,18 +2,17 @@
 #include "my_int.hpp"
 using namespace std;
 
-int reused = 42;
-
 int main()
 {
-	int a = 12, b = 24;
-	int *ptr = &a;
+	int i = 12;
+	int c = 42;
+	int *ptr = &i;
+	int *&ref = ptr;
 
-	cout << *ptr << endl;
-	*ptr = 42;
-	cout << a << endl;
-	ptr = &b;
-	cout << *ptr << endl;
+	cout << *ref << endl;
+	ref = &c;
+	*ref = 54;
+	cout << c << endl;
 	while (1);
 	return 0;
 }

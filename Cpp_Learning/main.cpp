f9f8cfdfcb1c49a7877a8ab13c1a64f5daf3d96c
Léo Garret <leodu34@hotmail.com>
2019-04-08 22:44:03 +0200

    Step: Adress exercice

--- Cpp_Learning/main.cpp
+++ Cpp_Learning/main.cpp
@@ -6,10 +6,14 @@ int reused = 42;
 
 int main()
 {
-	int a = 12;
-	int *p = &a;
+	int a = 12, b = 24;
+	int *ptr = &a;
 
-	cout << *p << endl;
+	cout << *ptr << endl;
+	*ptr = 42;
+	cout << a << endl;
+	ptr = &b;
+	cout << *ptr << endl;
 	while (1);
 	return 0;
 }

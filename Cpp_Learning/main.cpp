fa2adb3311ca22a107ae382f028d6ab164c6af9b
Léo Garret <leodu34@hotmail.com>
2019-04-15 21:13:06 +0200

    Update: change visual studio version (2015 to 2019)

--- Cpp_Learning/main.cpp
+++ Cpp_Learning/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "my_int.hpp"
 using namespace std;
 
 int main()
 {
-	const int a = 45; // Top level
-	const int *const pa = &a; // Top level
-	const int *pa = &a; // Low level
-	while (1);
+	int a = 5;
+
+	char* u = "leo";
+
+
 	return 0;
 }

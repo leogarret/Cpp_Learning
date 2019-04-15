da3d5fc80bebfd9cca981ce6724e15163d3fae5e
Léo Garret <leodu34@hotmail.com>
2019-04-15 23:20:18 +0200

    Step: auto, define and using

--- Cpp_Learning/main.cpp
+++ Cpp_Learning/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 using namespace std;
 
+using entier = int;
+
 int test()
 {
+	const int b = 13;
+	auto a = 13;
+
+	a = 23;
 	return rand();
 }
 
 int main()
 {
-
+	constexpr int c = 67;
+	constexpr const int *a = &c;
 	return 0;
 }

57161309ef26c0db67b7fc874b417b3d598ef556
Léo Garret <leodu34@hotmail.com>
2019-04-18 23:18:46 +0200

    Step: first little struct

--- Cpp_Learning/main.cpp
+++ Cpp_Learning/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-using namespace std;
 
-using entier = int;
-
-auto test()
-{
-	return 1345765999999999995;
-}
+typedef struct Sales_data {
+	std::string book_no;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
 
 int main()
 {
-	decltype(test()) i = 42;
+	int a = 3, b = 4;
+	decltype(a = b) c = a;
+	Sales_data data = { "AZ243", 12, 34.5 };
 
+	std::cout << data.book_no << std::endl;
+	getchar();
 	return 0;
 }

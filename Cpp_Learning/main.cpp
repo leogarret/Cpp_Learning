580dccbf9ac4f9d30228d5fa785c078dd7f39898
Léo Garret <leodu34@hotmail.com>
2019-04-20 22:50:28 +0200

    Step: iterator

--- Cpp_Learning/main.cpp
+++ Cpp_Learning/main.cpp
@@ -11,27 +11,14 @@ int strlen(char *str)
 
 int main()
 {
-	std::vector<std::string> tab;
-	std::string buff;
+	std::string word = "hello";
 
-	while (std::cin >> buff)
-		tab.push_back(buff);
-	for (auto &elem : tab) {
-		for (auto &child : elem) {
-			if (!std::isupper(child))
-				child = std::toupper(child);
-		}
+	if (word.begin() != word.end()) {
+		auto it = word.begin();
+		it++;
+		*it = std::toupper(*it);
 	}
-	unsigned i = 0;
-	for (auto elem : tab) {
-		if (i == 8) {
-			std::cout << std::endl;
-			i = 0;
-		}
-		std::cout << elem << " ";
-		++i;
-	}
-	std::cout << std::endl;
+	std::cout << word << std::endl;
 	std::getchar();
 	return 0;
 }

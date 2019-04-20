b5a22328d992c26c9b061e83a72ecf38b1db7c7a
Léo Garret <leodu34@hotmail.com>
2019-04-20 15:17:56 +0200

    Step: string concate

--- Cpp_Learning/main.cpp
+++ Cpp_Learning/main.cpp
@@ -2,12 +2,23 @@
 #include "Sales_item.h"
 #include <string>
 
+int strlen(char *str)
+{
+	int i = 0;
+	for (; str[i] != '\0'; ++i);
+	return (i);
+}
+
 int main()
 {
-	std::string s1("Hello");
-	s1 += " ca va ";
-	s1 += '?';
-	std::cout << s1;
-	getchar();
+	
+	std::string sentence;
+	std::string buff;
+
+	while (std::cin >> buff) {
+		sentence += buff;
+	}
+	std::cout << sentence;
+	while (1);
 	return 0;
 }

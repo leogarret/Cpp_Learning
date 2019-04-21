213cdd14e6bc20564a10410d1fb139026921e224
Léo Garret <leodu34@hotmail.com>
2019-04-21 23:15:11 +0200

    Step: array

--- Cpp_Learning/main.cpp
+++ Cpp_Learning/main.cpp
@@ -2,20 +2,19 @@
 #include <string>
 #include <vector>
 
-int strlen(char *str)
-{
-	int i = 0;
-	for (; str[i] != '\0'; ++i);
-	return (i);
-}
+int _arr[10];
 
 int main()
 {
-	std::vector<int> tab = { 12, 45, 32, 23, 98, 0, 32, 6 };
-	auto sit = tab.begin(), send = tab.end() - 1;
+	int arr[10] = {};
+	int arr2[10] = {};
 
-	for (; sit != send && sit < send; ++sit, --send)
-		std::cout << *sit + *send << std::endl;
+	for (int i = 0; i < 10; ++i)
+		arr[i] = i;
+	for (int i = 0; i < 10; ++i)
+		arr2[i] = arr[i];
+	for (auto elem : arr2)
+		std::cout << elem << std::endl;
 	std::getchar();
 	return 0;
 }

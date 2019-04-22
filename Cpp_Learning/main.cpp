e72048d35334461b22bc303cfa8ed1c12b61598c
Léo Garret <leodu34@hotmail.com>
2019-04-22 22:57:11 +0200

    Exercice: Comparing two arrays fo ints

--- Cpp_Learning/main.cpp
+++ Cpp_Learning/main.cpp
@@ -4,14 +4,23 @@
 
 int main()
 {
-	int arr[] = { 23, 45, 67, -45, 34, 32, -5, 8 };
-	int* ptr = arr;
-	int* ptr2 = std::end(arr);
+	int arr[] = { 23, 45, 67, -45, 34, 32, -5, 7 };
+	int arr2[] = { 23, 45, 67, -45, 34, 32, -5, 8 };
+	int* end = std::end(arr);
 
-	while (ptr < ptr2) {
-		std::cout << *ptr << std::endl;
-		++ptr;
+	if (std::size<int>(arr) == std::size<int>(arr2)) {
+		for (int i = 0; &arr[i] != end; ++i) {
+			if (arr[i] != arr2[i]) {
+				std::cout << "Arrays are not equals!" << std::endl;
+				std::getchar();
+				return 0;
+			}
+		}
+		std::cout << "Arrays are equals!" << std::endl;
+		std::getchar();
+		return 0;
 	}
+	std::cout << "Arrays are not equals!" << std::endl;
 	std::getchar();
 	return 0;
 }
